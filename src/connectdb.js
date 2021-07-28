4afb9a57051c7e88e3f64b7a67764a22db08dd70
qilip <qilip@qilip.io>
2021-07-28 08:18:17 +0000

    Add basic APIs

---
+++ src/connectdb.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+let conn = null;
+
+exports.connect = async () => {
+  if(conn == null){
+    conn = mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      bufferCommands: false,
+      serverSelectionTimeoutMS: 5000
+    }).then(() => mongoose);
+    await conn;
+  }
+  return conn;
+};
