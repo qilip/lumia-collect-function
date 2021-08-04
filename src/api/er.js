bc37e07af887f144b388b7ade3d86ded74433f54
qilip <qilip@qilip.io>
2021-08-04 09:57:03 +0000

    Release API

--- src/api/er.js
+++ src/api/er.js
@@ -35,53 +35,85 @@ er.interceptors.response.use(
 
 
 exports.getUserNum = async (nickname) => {
-  if(!nickname) return { 'code' : 400 };
+  if(!nickname) return { 'statusCode': 400, 'message': 'parameter empty' };
   try{
     const res = await er.get('/user/nickname', { params: { query: nickname } });
     console.log('getUserNum Response Time: ' + res.duration);
-    return res.data;
+    return {
+      'erCode': res.data.code,
+      'message': res.data.message,
+      'data': { 'user': res.data.user }
+    };
   }catch(e){
     console.error(e);
+    return {
+      'statusCode': 500,
+      'message': 'Lumia Collect server error'
+    };
   }
 };
 
 exports.getUserRank = async (userNum, seasonId) => {
-  if(!userNum || seasonId === undefined) return { 'code' : 400 };
+  if(!userNum || seasonId === undefined || seasonId === null)
+    return { 'statusCode': 400, 'message': 'parameter empty' };
   try{
     const res = await Promise.allSettled([
       er.get(`/rank/${userNum}/${seasonId}/1`),
       er.get(`/rank/${userNum}/${seasonId}/2`),
       er.get(`/rank/${userNum}/${seasonId}/3`)
     ]);
-    return res.map(
-      (res) => {
+    const data = res.map(
+      (res, idx) => {
         if(res.status === 'fulfilled'){
-          console.log('getUserRank Response Time:' + res.value.duration);
+          console.log('getUserRank[' + idx + '] Response Time:' + res.value.duration);
           return res.value.data;
         }else{
-          console.log('getUserRank Response Time:' + res.reason.duration);
-          return { 'code': 500 }; // TODO: 잘 처리하기
+          console.log('getUserRank[' + idx + '] Fail Response Time:' + res.reason.duration);
+          return { 'code': 500, 'message': 'axios request fail' };
         }
       }
     );
+    return {
+      'statusCode': 200,
+      'message': 'Success',
+      'data': {
+        seasonId,
+        'solo': data[0],
+        'duo': data[1],
+        'squad': data[2]
+      }
+    };
   }catch(e){
     console.error(e);
+    return {
+      'statusCode': 500,
+      'message': 'Lumia Collect server error'
+    };
   }
 };
 
 exports.getUserStats = async (userNum, seasonId) => {
-  if(!userNum || seasonId === undefined) return { 'code' : 400 };
+  if(!userNum || seasonId === undefined || seasonId === null)
+    return { 'statusCode': 400, 'message': 'parameter empty' };
   try{
     const res = await er.get(`user/stats/${userNum}/${seasonId}`);
     console.log('getUserStats Response Time: ' + res.duration);
-    return res.data;
+    return {
+      'erCode': res.data.code,
+      'message': res.data.message,
+      'data': { 'userStats': res.data.userStats }
+    };
   }catch(e){
     console.error(e);
+    return {
+      'statusCode': 500,
+      'message': 'Lumia Collect server error'
+    };
   }
 };
 
 exports.getUserGames = async (userNum, start) => {
-  if(!userNum) return { 'code' : 400 };
+  if(!userNum) return { 'statusCode': 400, 'message': 'parameter empty' };
   try{
     let res;
     if(start){
@@ -90,62 +122,95 @@ exports.getUserGames = async (userNum, start) => {
       res = await er.get('/user/games/' + userNum);
     }
     console.log('getUserGames Response Time: ' + res.duration);
-    return res.data;
+    return {
+      'erCode': res.data.code,
+      'message': res.data.message,
+      'data': { 'games': res.data.userGames }
+    };
   }catch(e){
     console.error(e);
+    return {
+      'statusCode': 500,
+      'message': 'Lumia Collect server error'
+    };
   }
 };
 
 exports.getGame = async (gameId) => {
-  if(!gameId) return { 'code' : 400 };
+  if(!gameId) return { 'statusCode': 400, 'message': 'parameter empty' };
   try{
     const res = await er.get('/games/' + gameId);
     console.log('getGame Response Time: ' + res.duration);
-    return res.data;
+    return {
+      'erCode': res.data.code,
+      'message': res.data.message,
+      'data': { 'games': res.data.userGames }
+    };
   }catch(e){
     console.error(e);
+    return {
+      'statusCode': 500,
+      'message': 'Lumia Collect server error'
+    };
   }
 };
 
 exports.getRoute = async (routeId) => {
-  if(!routeId) return { 'code' : 400 };
+  if(!routeId) return { 'statusCode': 400, 'message': 'parameter empty' };
   try{
     const res = await er.get('/weaponRoutes/recommend/' + routeId);
     console.log('getRoute Response Time: ' + res.duration);
-    return res.data;
+    return {
+      'erCode': res.data.code,
+      'message': res.data.message,      
+      'data': { 'route': res.data.result }
+    };
   }catch(e){
     console.error(e);
+    return {
+      'statusCode': 500,
+      'message': 'Lumia Collect server error'
+    };
   }
 };
 
 
 exports.getUserRecentGames = async (userNum, start, limit) => {
-  if(!userNum || !limit) return { 'code' : 400 };
+  if(!userNum || !limit) return { 'statusCode': 400, 'message': 'parameter empty' };
   try{
     let games = [];
     let next = start;
     let i = 0;
+    let res;
     while(games.length < limit && next !== -1 && i < limit){
-      let res;
       if(next){
         res = await er.get('/user/games/' + userNum, { params: { next } });
       }else{
         res = await er.get('/user/games/' + userNum);
       }
       console.log('getUserGames[' + (i/10) + '] Response Time: ' + res.duration);
+      if(res.data.code !== 200) return { 'erCode': res.data.code, 'message': res.data.message };
       games.push(...res.data.userGames);
       next = res.data.next || -1;
       i += 10;
     }
-    
-    return games;
+    return {
+      'erCode': res.data.code,
+      'message': res.data.message,
+      'data': { 'games': res.data.userGames }
+    };
   }catch(e){
     console.error(e);
+    return {
+      'statusCode': 500,
+      'message': 'Lumia Collect server error'
+    };
   }  
 };
 
 exports.getUserSeason = async (userNum, seasonId) => {
-  if(!userNum || seasonId === undefined) return { 'code' : 400 };
+  if(!userNum || seasonId === undefined || seasonId === null)
+    return { 'statusCode': 400, 'message': 'parameter empty' };
   try{
     const res = await Promise.allSettled([
       er.get(`/rank/${userNum}/${seasonId}/1`),
@@ -159,26 +224,27 @@ exports.getUserSeason = async (userNum, seasonId) => {
           console.log('getUserSeason[' + idx + '] Response Time:' + res.value.duration);
           return res.value.data;
         }else{
-          console.log('getUserSeason[' + idx + '] Response Time:' + res.reason.duration);
-          console.log(res.reason);
-          return { 'code': 500 }; // TODO: 잘 처리하기
+          console.log('getUserSeason[' + idx + '] Fail Response Time:' + res.reason.duration);
+          return { 'code': 500, 'message': 'axios request fail' };
         }
       }
     );
-    const rankData = data.slice(0, 3).map(
-      (data, idx) => {
-        return {
-          seasonId,
-          matchingTeamMode: idx+1,
-          mmr: data.userRank.mmr,
-          rank: data.userRank.rank,
-        };
+    return {
+      'statusCode': 200,
+      'message': 'Success',
+      'data': {
+        seasonId,
+        'solo': data[0],
+        'duo': data[1],
+        'squad': data[2],
+        'userStats': data[3]
       }
-    );
-    const userRank = [ ...rankData ];
-    const userStats = data[3].userStats || [];
-    return { userRank, userStats };
+    };
   }catch(e){
     console.error(e);
+    return {
+      'statusCode': 500,
+      'message': 'Lumia Collect server error'
+    };
   }
 };
--- src/collect.js
+++ src/collect.js
@@ -1,31 +1,40 @@
 const er = require('./api/er.js');
-const ctrl = require('./api/ctrl.js');
+// const ctrl = require('./api/ctrl.js');
 
 const funcMapper = {
   getUserNum: p => er.getUserNum(p.nickname),
   getUserRank: p => er.getUserRank(p.userNum, p.seasonId),
   getUserStats: p => er.getUserStats(p.userNum, p.seasonId),
   getUserGames: p => er.getUserGames(p.userNum, p.start),
-  getGame: p => ctrl.getGame(p.gameId),
-  getRoute: p => ctrl.getRoute(p.routeId),
+  getGame: p => er.getGame(p.gameId),
+  getRoute: p => er.getRoute(p.routeId),
   getUserRecentGames: p => er.getUserRecentGames(p.userNum, p.start, p.limit),
-  getUserSeason: p => ctrl.getUserSeason(p.userNum, p.seasonId),
-  getUserInfo: p => ctrl.getUserInfo(p.nickname, p.seasonId)
+  getUserSeason: p => er.getUserSeason(p.userNum, p.seasonId),
+  // getUserInfo: p => ctrl.getUserInfo(p.nickname, p.seasonId)
 };
 
 module.exports.handler = async (event, ctx) => {
   ctx.callBackWaitsForEmptyEventLoop = false;
   
   if(!event.query || !event.param)
-    return { 'statusCode': 400, 'body': 'Missing query' };
+    return { 'statusCode': 400, 'message': 'Missing query' };
   if(!funcMapper.hasOwnProperty(event.query))
-    return { 'statusCode': 400, 'body': 'Bad query' };
+    return { 'statusCode': 400, 'message': 'Bad query' };
   if(process.env.STAGE === 'dev') console.log(event);
 
   try{
     const res = await funcMapper[event.query](event.param);
-    return { 'statusCode': 200, 'body': res };
+    return {
+      'statusCode': res.statusCode || 200,
+      'erCode': res.erCode,
+      'message': res.message || 'Success',
+      'data': res.data
+    };
   }catch(e){
-    ctx.captureError(e);
+    console.error(e);
+    return {
+      'statusCode': 500,
+      'message': 'Lumia Collect server error'
+    };
   }
 };
